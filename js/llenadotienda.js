2dfc0de6c40f4ab6b733e64a9c7ee8e1dba10021
Gelus0926 <mateofrako@gmail.com>
2022-02-28 19:25:57 -0500

    se agregan las imagenes, y los productos

--- js/llenadotienda.js
+++ js/llenadotienda.js
@@ -1,10 +1,16 @@
 //creando un arreglo de objetos
 
 let productos = [
-
-    {nombre:"camiseta",precio:80000,descripcion:"camiseta de deadpool en varias tallas"},
-    {nombre:"figura de accion",precio:150000,descripcion:"figura de accion de deadpool"},
-
+    {nombre:"Camiseta",precio:45000,descripcion:"Camiseta Demon Slayer"},
+    {nombre:"Camiseta",precio:45000,descripcion:"Camiseta jujutsu Kaisen"},
+    {nombre:"Camiseta",precio:45000,descripcion:"Camiseta Nanatsu No Taizai"},
+    {nombre:"Camiseta",precio:45000,descripcion:"Camiseta One Piece"},    
+    {nombre:"Chompa",precio:90000,descripcion:"Chompa Boku No Hero Academia"},
+    {nombre:"Chompa",precio:90000,descripcion:"Chompa Bleach"},
+    {nombre:"Chompa",precio:90000,descripcion:"Chompa Death Note"},
+    {nombre:"Buzo",precio:75000,descripcion:"Buzo Shingeki No Kyojin"},
+    {nombre:"Buzo",precio:75000,descripcion:"Buzo Fairy Tail"},
+    {nombre:"Buzo",precio:75000,descripcion:"Buzo Fullmeta Alchemist"}
 ]
 
 console.log(productos)
